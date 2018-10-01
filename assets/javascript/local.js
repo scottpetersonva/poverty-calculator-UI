287cbc144132be3c53aa0d5b5eb58d1f832a7da3
Scott Peterson <scott.peterson.va@gmail.com>
2018-10-01 12:40:44 -0400

    made results expandable

--- assets/javascript/local.js
+++ assets/javascript/local.js
@@ -1,9 +1,14 @@
 $( document ).ready(function(){
   $('.collapsible').collapsible();
-  $('.slider').slider();
-  $('.modal').modal();
+  // $('.slider').slider();
+  // $('.modal').modal();
+  $('.tabs').tabs();
   $('.button-collapse').sideNav({
         closeOnClick: true
-      }
+      }     
     );
+    var elem = document.querySelector('.collapsible.expandable');
+      var instance = M.Collapsible.init(elem, {
+        accordion: false
+      });
   });
